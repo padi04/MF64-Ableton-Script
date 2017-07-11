487095849cb4c762fbee60c22d4436ad8c1aa49e
padi04 <matiasjpadilla@gmail.com>
2017-07-11 00:36:53 -0300

    Detection update

--- Midi_Fighter_64/__init__.py
+++ Midi_Fighter_64/__init__.py
@@ -1,10 +1,9 @@
+from _Framework.Capabilities import CONTROLLER_ID_KEY, PORTS_KEY, NOTES_CC, SCRIPT, SYNC, controller_id, inport, outport
 from Midi_Fighter_64 import Midi_Fighter_64
 
 def create_instance(c_instance):
 	return Midi_Fighter_64(c_instance)
 
-from _Framework.Capabilities import *
-
 def get_capabilities():
-	return {CONTROLLER_ID_KEY: controller_id(vendor_id=2580, product_ids=[0008], model_name='Midi Fighter 64'),
-	 PORTS_KEY: [inport(props=[NOTES_CC, SCRIPT, REMOTE]), outport(props=[SCRIPT, REMOTE])]}
+	return {CONTROLLER_ID_KEY: controller_id(vendor_id=2580, product_ids=[8], model_name='Midi Fighter 64'),
+	 PORTS_KEY: [inport(props=[NOTES_CC, SCRIPT]), outport(props=[NOTES_CC, SYNC, SCRIPT])]}
